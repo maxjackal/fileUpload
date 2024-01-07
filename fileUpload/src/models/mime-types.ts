128fc8578685042a0b7855c23153251fd792fba9
maxjackal <74346691+maxjackal@users.noreply.github.com>
2024-01-07 05:43:43 +0300

    Update mime-types.ts

--- fileUpload/src/models/mime-types.ts
+++ fileUpload/src/models/mime-types.ts
@@ -1,8 +1,11 @@
-// mime-types.ts
 export const MIME_TYPE_MAP: { [key: string]: string } = {
     'image/jpeg': 'jpg',
+    'image/jpeg': 'jpeg',
     'image/png': 'png',
     'application/pdf': 'pdf',
+    'application/zip': 'zip',
+    'application/msword':'doc',
+  
     // Diğer MIME türleri ekleyebilirsiniz.
   };
   
